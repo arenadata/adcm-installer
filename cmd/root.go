47b669a8eae06705654a7b9ebad3a95a66eb1f92
Aleksey Shulutkov <a.shulutkov@arenadata.io>
2024-12-25 18:24:36 +0700

    change app name

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,15 @@
 package cmd
 
 import (
+	"os"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "github.com/arenadata/adcm-installer",
+	Use:   "adcm",
 	Short: "A brief description of your application",
 }
 
